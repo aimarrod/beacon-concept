f08703e88f671fdfe12f92bf74fb73c3e6c2ad73
julen30 <julen.zugasti.bilbao@gmail.com>
2015-07-17 16:22:18 +0200

    Material design seek bar

--- app/src/main/java/com/olbalabs/beaconconcept/DistanceActivity.java
+++ app/src/main/java/com/olbalabs/beaconconcept/DistanceActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 
+import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
+
 /**
  * Created by julenzugastibilbao on 17/7/15.
  */
@@ -13,5 +15,7 @@ public class DistanceActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_distance);
+
+
     }
 }
